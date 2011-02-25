dd6afa903506f41cc99af543a60369a3d0d670e5
ldmotta <ldmotta@visie.com.br>
2011-02-25 16:48:43 -0300

    FIRST COMMIT
    Adicionndo os arquivos da biblioteca de integração cobredireto

---
+++ CD_config.php
@@ -0,0 +1,16 @@
+<?php
+//Ambiente de utilização do CobreDireto
+define('CD_AMBIENTE'    , 'teste');
+//Código da loja junto ao CobreDireto
+define('CD_CODLOJA'     , 'visie'); 
+//Usuario no CobreDireto
+define('CD_USUARIO'     , 'visie_ws');
+//Senha no CobreDireto
+define('CD_SENHA'       , '12345');
+//URL de Recibo
+define('CD_URL_RECIBO'  ,'http://sigrido.boldcron.com.br/recibo.php');
+//URL em caso de erro
+define('CD_URL_ERRO'    ,'http://sigrido.boldcron.com.br/erro.php');
+//URL de Retorno para o Bell e o Probe
+define('CD_URL_RETORNO' ,'http://sigrido.boldcron.com.br/retorno.php');
+?>
